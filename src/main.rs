c715660f3b8bbeaafd721469044811a073c23e05
Heshan Padmasiri <heshanp@wso2.com>
2024-08-23 05:23:21 +0530

    Refactor old code to be nice

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,115 @@
 use git2::{Repository, RepositoryState};
+use std::path::{Path, PathBuf};
+use std::{env, fs};
 
-fn main() {
-    // FIXME: can't use "~/Projects/ballerina-lang" here
-    let TEST_REPO = "/Users/heshanpadmasiri/Projects/ballerina-lang";
-    let repo = match Repository::open(TEST_REPO) {
-        Ok(repo) => repo,
-        Err(e) => panic!("failed to open {}", e)
-    };
-    if repo.state() != RepositoryState::Clean {
-        panic!("Repository state is not clean");
+#[derive(Debug)]
+struct TestCommand {
+    start: String,
+    end: String,
+    command: String,
+}
+
+#[derive(Debug)]
+enum Action {
+    Test(TestCommand),
+}
+#[derive(Debug)]
+struct Args {
+    path: PathBuf,
+    action: Action,
+}
+
+fn main() -> Result<(), String> {
+    match parse_args() {
+        Ok(Args { path, action }) => execute_action(&path, action),
+        Err(err) => Err(format!("failed to parse arguments due to {err}")),
     }
-    let start_commit = "e27ccd56";
-    let end_commit = "8f9b0527";
+}
 
-    let mut revwalk = match repo.revwalk() {
-        Ok(revwalk) => revwalk,
-        Err(e) => panic!("failed to create revwalk: {}", e),
-    };
+fn execute_action(path: &Path, action: Action) -> Result<(), String> {
+    match action {
+        Action::Test(TestCommand {
+            start,
+            end,
+            command,
+        }) => execute_test(path, &command, &start, &end),
+    }
+}
+
+fn execute_test(
+    path: &Path,
+    _command: &str,
+    start_commit: &str,
+    end_commit: &str,
+) -> Result<(), String> {
+    // TODO: given walking the commit history is common factor this to a seperate object that can
+    // take actions and initialized with path, commit range
+    let repo = Repository::open(path).map_err(|_| "failed to open repository")?;
+    if repo.state() != RepositoryState::Clean {
+        return Err(String::from(
+            "repository is not clean, comit any changes before running gitwalker",
+        ));
+    }
+
+    let mut revwalk = repo
+        .revwalk()
+        .map_err(|err| format!("failed to create a revwalk due to {err}"))?;
 
     if let Err(e) = revwalk.push_range(&format!("{}..{}", start_commit, end_commit)) {
-        panic!("failed to set revwalk range: {}", e);
-    }
+        return Err(format!("failed to set revwalk range: {}", e));
+    };
 
     for commit_id in revwalk {
-        match commit_id {
-            Ok(commit_id) => {
-                let commit = match repo.find_commit(commit_id) {
-                    Ok(commit) => commit,
-                    Err(e) => panic!("failed to find commit: {}", e),
-                };
-                // TODO: checkout commit
-                // TODO: run the given command at the commit and check the status code
-                println!("{}", commit.id());
-            }
-            Err(e) => panic!("failed to iterate over commits: {}", e),
+        let commit_id =
+            commit_id.map_err(|e| format!("failed to iterate over commits due to {e}"))?;
+        let commit = repo
+            .find_commit(commit_id)
+            .map_err(|e| format!("failed to find commit due to {e}"))?;
+        dbg!(commit_id, commit);
+    }
+    Ok(())
+}
+
+fn parse_args() -> Result<Args, String> {
+    let args: Vec<String> = env::args().collect();
+
+    match args.as_slice() {
+        [_, test, path, start_commit, end_commit, remaining_args @ ..] if test == "test" => {
+            let path = validate_and_get_absolute_path(path)?;
+            finish_parsing_test_args(remaining_args, path, start_commit, end_commit)
         }
+        _ => Err(String::from("invalid comand")),
+    }
+}
+
+fn validate_and_get_absolute_path(path_str: &str) -> Result<PathBuf, String> {
+    let path = Path::new(path_str);
+    let path = if path.is_absolute() {
+        path.to_path_buf()
+    } else {
+        let current_dir = env::current_dir()
+            .map_err(|err| format!("failed to get current directory due to {err}"))?;
+        current_dir.join(path)
+    };
+    fs::canonicalize(path).map_err(|err| format!("invalid path {err}"))
+}
+
+fn finish_parsing_test_args(
+    remaining_args: &[String],
+    path: PathBuf,
+    start_commit: &str,
+    end_commit: &str,
+) -> Result<Args, String> {
+    match remaining_args {
+        [] => Err(String::from("expected a command to test")),
+        [command] => Ok(Args {
+            path,
+            action: Action::Test(TestCommand {
+                start: start_commit.to_string(),
+                end: end_commit.to_string(),
+                command: command.to_string(),
+            }),
+        }),
+        _ => Err(String::from("too many arguments")),
     }
 }
